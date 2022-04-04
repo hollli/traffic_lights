43fc689e6060f423dc5202d10855239a92c7eed6
admin <619metal619@gmail.com>
2022-04-04 07:48:44 +0300

    Initial commit

---
+++ js.js
@@ -0,0 +1,43 @@
+// function run() {
+//     $('#result').empty();
+//     for (let timer = 0; timer < 1000; timer++) {
+//         if(timer>0&&timer<200){
+//             document.getElementById("red").style.backgroundColor = "#ff0000";
+//             document.getElementById("yellow").style.backgroundColor = "#b1be00";
+//             document.getElementById("green").style.backgroundColor = "#168400";
+//         }
+//         if(timer>200&&timer<400){
+//             document.getElementById("red").style.backgroundColor = "#ff0000";
+//             document.getElementById("yellow").style.backgroundColor = "#ffff00";
+//             document.getElementById("green").style.backgroundColor = "#168400";
+//         }
+//         if(timer>400&&timer<600){
+//             document.getElementById("red").style.backgroundColor = "#7f000c";
+//             document.getElementById("yellow").style.backgroundColor = "#b1be00";
+//             document.getElementById("green").style.backgroundColor = "#00ff00";
+//         }
+//         if(timer>1&&timer<1000){
+//             document.getElementById("red").style.backgroundColor = "#7f000c";
+//             document.getElementById("yellow").style.backgroundColor = "#b1be00";
+//             document.getElementById("green").style.backgroundColor = "#00ff00";
+//             // if(timer % 2 === 0) {
+//             //     document.getElementById("green").style.backgroundColor = "#00ff00";
+//             // }
+//             if(timer % 2 !== 0) {
+//                 document.getElementById("green").style.backgroundColor = "#168400";
+//
+//
+//             }
+//             break;
+//
+//         }
+//         if(timer>800){
+//             document.getElementById("red").style.backgroundColor = "#7f000c";
+//             document.getElementById("yellow").style.backgroundColor = "#ffff00";
+//             document.getElementById("green").style.backgroundColor = "#168400";
+//
+//         }
+//     }
+//
+// }
+// setInterval(run, 1000);
